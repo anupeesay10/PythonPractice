e5e344495d4389f7b62861fbfdcdfaddb74c1df0
anupeesay10 <anupeesay@gmail.com>
2023-08-08 17:10:26 -0400

    Addded winter problem changes

--- Using files/winter_python_problems_#4.py
+++ Using files/winter_python_problems_#4.py
@@ -1,12 +1,12 @@
 def average_of_scores(file_name):
+
     f = open(file_name, "r")
     for line in f.readlines(): #should I use f.readlines or f.readline or f.read?
         for character in line:
             if ord(character) >= 97 and ord(character) <= 122 or ord(character) >= 65 and ord(character) <= 90:
-
-
-
-
+                continue
+            if ord(character) >= 0 and ord(character) <= 57:
+                print(character, end = "") #How do I get the averages now?
 
 
 
---
+++ Using files/winter_python_problems_SDL_#1.py
@@ -0,0 +1,40 @@
+def largest(l):
+    char = 0
+    for character in l.split(","):
+        # print(int(character))
+        if int(character) > char:
+            char = int(character)  #Why does this not work the other way around?
+    return char
+
+def smallest(s):
+# smol = 0
+    # for character in s.split(","):
+    #     # print(int(character))
+    #     if int(character) < smol:
+    #         smol = int(character)
+    # return smol
+
+def average(a):
+    s = 0
+    for character in a.split(","):
+        # print(int(character))
+        s = s + int(character)
+    count = 1
+
+    for character in a:
+        if character == ",":
+            count = count + 1
+
+    mean =  s / count
+    return mean
+
+
+
+if __name__ == "__main__":
+    list = input("Enter a list of integers separated by commas:")
+    large_o_list = largest(list)
+    print("The largest number is: %d." %(large_o_list))
+    smol_o_list = smallest(list)
+    print("The smallest number is: %d." %(smol_o_list))
+    avg_o_list = average(list)
+    print("The average of the list is: %f" %(avg_o_list))
